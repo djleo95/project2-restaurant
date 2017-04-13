feca90ca22c27962d5a2eab322e3af8ac8b13cb8
dat-hedspi <dat.hedspi@gmail.com>
2017-04-13 16:39:59 +0700

    Order Combos

---
+++ app/controllers/order_combos_controller.rb
@@ -0,0 +1,29 @@
+class OrderCombosController < ApplicationController
+  def create
+    @order = current_order
+    @order_combo = @order.order_combos.new order_combo_params
+    @order.save
+    session[:order_id] = @order.id
+  end
+
+  def update
+    @order = current_order
+    @order_combo = @order.order_combos.find_by id: params[:id]
+    @order_combo.update_attributes order_combo_params
+    @order_combos = @order.order_combos
+    @order_dishes = @order.order_dishes
+  end
+
+  def destroy
+    @order = current_order
+    @order_combo = @order.order_combos.find_by id: params[:id]
+    @order_combo.destroy
+    @order_combos = @order.order_combos
+    @order_dishes = @order.order_dishes
+  end
+
+  private
+  def order_combo_params
+    params.require(:order_combos).permit :quantity, :id, :combo_id
+  end
+end
