d7088b4993d316ca85008d227b8effa6f5c7ed73
dat-hedspi <dat.hedspi@gmail.com>
2017-04-17 22:25:45 +0700

    Update logic search order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,13 @@ class OrdersController < ApplicationController
   def index
     if params[:order]
       order = Order.find_by code: params[:order][:code]
-      session[:order_id] = order.id
-      redirect_to order_path
+      if order.guest != nil && order.guest.email == params[:order][:email]
+        session[:order_id] = order.id
+        redirect_to order_path
+      else
+        flash[:danger] = "There are no order the same as the infomation!"
+        render :index
+      end
     end
   end
 end
